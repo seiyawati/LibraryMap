4683935ffe1bc838ab54ed4dc78d1be03822142b
mi-wada <mitsu.lol.0223@gmail.com>
2020-09-23 15:01:26 +0900

    住所，営業時間を正しく表示． 新規検索時にdiv要素を初期化．

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-var libraryInfomationView = "\n<div class=\"p-2 library-info\">\n<a href=\"{url}\" class=\"library-name\" target=\"_blank\">{library_name}</a>\n<p class=\"\">\u56F3\u66F8\u9928\u306E\u4F4F\u6240\u56F3\u66F8\u9928\u306E\u4F4F\u6240\u56F3\u66F8\u9928\u306E\u4F4F\u6240</p>\n<div class=\"row no-gutters\">\n<div class=\"col-12 col-md-6 text-center pr-2\"><img src=\"{image_url}\" class=\"w-100\"></div>\n<div class=\"col-12 col-md-6 p-0\">\n<ul class=\"mb-2\">\n<li><i class=\"far fa-clock mr-2\"></i><span class=\"\">10:00-19:00</span></li>\n<li><i class=\"far fa-calendar-times mr-2\"></i><span class=\"\">\u5E74\u672B\u5E74\u59CB</span></li>\n</ul>\n<button class=\"p-2 root-btn\"><i class=\"fas fa-route mr-2\"></i>\u30EB\u30FC\u30C8\u3092\u8ABF\u3079\u308B</button>\n</div>\n</div>\n";
+var libraryInfomationView = "\n<div class=\"p-2 library-info\">\n<a href=\"{url}\" class=\"library-name\" target=\"_blank\">{library_name}</a>\n<p class=\"\">{address}</p>\n<div class=\"row no-gutters\">\n<div class=\"col-12 col-md-6 text-center pr-2\"><img src=\"{image_url}\" class=\"w-100\"></div>\n<div class=\"col-12 col-md-6 p-0\">\n<ul class=\"mb-2\">\n<li><i class=\"far fa-clock mr-2\"></i><span class=\"\">{open}-{close}</span></li>\n<li><i class=\"far fa-calendar-times mr-2\"></i><span class=\"\">\u5E74\u672B\u5E74\u59CB</span></li>\n</ul>\n<button class=\"p-2 root-btn\"><i class=\"fas fa-route mr-2\"></i>\u30EB\u30FC\u30C8\u3092\u8ABF\u3079\u308B</button>\n</div>\n</div>\n";
 $(function () {
     $('#search_books').click(function () {
+        //要素を空にする
+        $(".library-infos").empty();
         //検索フォームから入力を取得
         var bookNames = $('#book_names').val().split(',');
         //近隣の図書館で，蔵書が有る図書館を取得
@@ -15,19 +17,20 @@ $(function () {
                 librarys[isbn]['librarys'][i]['image_url'] = getImage(libraryName + '+' + '外観');
                 $('.library-infos').append(libraryInfomationView.replace('{url}', librarys[isbn]['librarys'][i]['url'])
                     .replace('{library_name}', librarys[isbn]['librarys'][i]['libraryName'])
-                    .replace('{image_url}', librarys[isbn]['librarys'][i]['image_url']));
+                    .replace('{image_url}', librarys[isbn]['librarys'][i]['image_url'])
+                    .replace('{address}', librarys[isbn]['librarys'][i]['address'])
+                    .replace('{open}', hhmm(librarys[isbn]['librarys'][i]['opening_hours']['periods'][0]['open']['time']))
+                    .replace('{close}', hhmm(librarys[isbn]['librarys'][i]['opening_hours']['periods'][0]['close']['time'])));
             }
         }
         //マップにピンを立てる
         pinMap();
     });
-
     //ルート検索のボタンがクリックされた時
-    $(document).on('click', '.root-btn', function() {
-        var library = $(this).parents('.library-info').find('.library-name').text()
+    $(document).on('click', '.root-btn', function () {
+        var library = $(this).parents('.library-info').find('.library-name').text();
         searchRoute(library);
-    })
-    
+    });
     //初期表示でマップに現在位置を表示
     window.onload = initMap();
 });
--- ts/google.ts
+++ ts/google.ts
@@ -35,3 +35,7 @@ function getImage(query: string): string {
   const imageURL: string = response.items[0].link;
   return imageURL;
 }
+
+function hhmm(str) {
+  return str.substr(0, 2) + ':' + str.substr(2, 2);
+}
