731dc4f57cfedcbca9be7797ff65a1a8cb5fe300
mi-wada <mitsu.lol.0223@gmail.com>
2020-09-24 14:38:24 +0900

    ローディングを表示するように修正

--- ts/main.ts
+++ ts/main.ts
@@ -5,31 +5,33 @@ $(function () {
     $("#loading").show();
     //要素を空にする
     $(".library-infos").empty();
-    //検索フォームから入力を取得
-    var bookNames = $('#book_names').val().split(',');
-    //近隣の図書館で，蔵書が有る図書館を取得
-    var librarys = searchLibrarys(bookNames, getUserPosition());
-    for (var isbn in librarys) {
-      let libraryNamesSplitByComma = "";
-      let querySplitByComma = "";
-        for (var i in librarys[isbn]['librarys']) {
-            var libraryName = librarys[isbn]['librarys'][i]['libraryName'];
-            libraryNames.push(libraryName);
-            libraryNamesSplitByComma += libraryName + ',';
-            querySplitByComma += libraryName + '+' + '外観' + ',';
-        }
-        const openingHoursArray = getOpeningHours(libraryNamesSplitByComma);
-        const imageURLs = getImage(querySplitByComma);
-        for (var i in librarys[isbn]['librarys']) {
-          librarys[isbn]['librarys'][i]['opening_hours'] = openingHoursArray[i];
-          librarys[isbn]['librarys'][i]['image_url'] = imageURLs[i];
-          $('.library-infos').append(formatLibraryInfo(librarys[isbn]['librarys'][i]));
-        }
-    }
-    //ローディング画面OFF
-    $("#loading").hide();
-    //マップにピンを立てる
-    pinMap();
+    setTimeout(function(){
+      //検索フォームから入力を取得
+      var bookNames = $('#book_names').val().split(',');
+      //近隣の図書館で，蔵書が有る図書館を取得
+      var librarys = searchLibrarys(bookNames, getUserPosition());
+      for (var isbn in librarys) {
+        let libraryNamesSplitByComma = "";
+        let querySplitByComma = "";
+          for (var i in librarys[isbn]['librarys']) {
+              var libraryName = librarys[isbn]['librarys'][i]['libraryName'];
+              libraryNames.push(libraryName);
+              libraryNamesSplitByComma += libraryName + ',';
+              querySplitByComma += libraryName + '+' + '外観' + ',';
+          }
+          const openingHoursArray = getOpeningHours(libraryNamesSplitByComma);
+          const imageURLs = getImage(querySplitByComma);
+          for (var i in librarys[isbn]['librarys']) {
+            librarys[isbn]['librarys'][i]['opening_hours'] = openingHoursArray[i];
+            librarys[isbn]['librarys'][i]['image_url'] = imageURLs[i];
+            $('.library-infos').append(formatLibraryInfo(librarys[isbn]['librarys'][i]));
+          }
+      }
+      //ローディング画面OFF
+      $("#loading").hide();
+      //マップにピンを立てる
+      pinMap();
+    },100);
   });
 
   //ルート検索のボタンがクリックされた時
